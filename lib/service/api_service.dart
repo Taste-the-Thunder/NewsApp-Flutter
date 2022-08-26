8f7dc317b751fa10da6276d5094f84e2b5e6ad20
Dhruvin <35141691+dhruvinprajapati@users.noreply.github.com>
2022-08-27 01:08:18 +0530

    first commit

---
+++ lib/service/api_service.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:newsapi/model/Article.dart';
+
+class ApiService{
+
+  final endPoint = "https://newsapi.org/v2/everything?q=tesla&from=2022-07-26&sortBy=publishedAt&apiKey=27aafa002a324c3fb9d80fe2f2c69977";
+  Future<List<Article>> getArticle() async{
+      Response response = await get(Uri.parse(endPoint));
+      if(response.statusCode == 200){
+
+        Map<String, dynamic> json = jsonDecode(response.body);
+        List<dynamic> body = json['articles'];
+        List<Article> articles = body.map((dynamic item) => Article.fromJson(item)).toList();
+        return articles;
+
+      } else {
+        throw 'Data not fount';
+      }
+  }
+}
